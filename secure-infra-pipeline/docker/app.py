d9106e7be233cd94454e2bf0632d5834c3e179b6
Sayan Singh <157497677+singhsayan@users.noreply.github.com>
2025-07-20 11:31:23 +0530

    Delete secure-infra-pipeline directory

--- secure-infra-pipeline/docker/app.py
+++
@@ -1,1 +0,0 @@
-print("Secure DevSecOps Pipeline running successfully!")
--- secure-infra-pipeline/terraform/output.tf
+++
@@ -1,9 +0,0 @@
-output "s3_bucket_name" {
-  value = aws_s3_bucket.secure_bucket.id
-}
-
-output "dynamo_table_name" {
-  value = aws_dynamodb_table.secure_table.name 
-} 
-
-
